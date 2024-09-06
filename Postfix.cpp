8c3cbe25de6283e1d5c1c84e2bfa5ec8d4526ab4
KevinHendersonCSUF <123698269+KevinHendersonCSUF@users.noreply.github.com>
2024-09-05 22:23:33 -0700

    Created Postfix.cpp and added header

---
+++ Postfix.cpp
@@ -0,0 +1,7 @@
+//-------------------------------------------------------------------------------------------------------------
+// Group names: Kevin Henderson,
+// Assignment : No.1
+// Due date : 9/11/24
+// Purpose: this program reads an expression in postfix form, evaluates the expression
+// and displays its value
+//-----------------------------------------------------------------------------------------------------------------
